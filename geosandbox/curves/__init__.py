f7fd15be9663680d0aa00e71ba105d412e49e9b2
wh1t3a <imaosorov.tb@dvfu@.ru>
2026-07-21 09:55:00 +0800

    feat(curves): add Bezier curves via de Casteljau algorithm
    Introduces the curves subpackage with ControlPoint, lerp/distance helpersand the de Casteljau evaluation routine.

---
+++ geosandbox/curves/__init__.py
@@ -0,0 +1,15 @@
+﻿"""Curve Algorithms Module"""
+
+from .bezier import (
+    ControlPoint,
+    lerp_point,
+    distance,
+    bezier,
+)
+
+__all__ = [
+    "ControlPoint",
+    "lerp_point",
+    "distance",
+    "bezier",
+]
